69fcc41879db8fcb3578e954da533ce2254fce03
Phuongpiper <nguyenhongphuong.vn1@gmail.com>
2023-09-07 18:37:04 +0700

    Load câu trả lời

--- duantotnghiep/src/main/resources/static/js/TracNghiem-ctrl.js
+++ duantotnghiep/src/main/resources/static/js/TracNghiem-ctrl.js
@@ -6,10 +6,37 @@ app.controller("TracNghiem-ctrl", function($scope, $http, $window) {
         // Gọi API để tải dữ liệu câu hỏi từ đường dẫn "/rest/TracNghiem"
         $http.get("/rest/tracnghiem").then(resp => {
             $scope.itemsCauHoi = resp.data;
-            console.log("API Response:", resp.data);
+            console.log("API Response:", resp.data)
+
+              // Tạo mảng mới chứa thông tin câu hỏi, câu trả lời và mảng đáp án
+              const questionAnswerArray = [];
+
+              for (const item of $scope.itemsCauHoi) {
+                  if (item.cauTraLoi) {
+                      const splitAnswers = item.cauTraLoi.split(',').map(answer => answer.trim());
+  
+                      // Tạo đối tượng chứa thông tin và đưa vào mảng mới
+                      const questionObject = {
+                          cauHoi: item.cauHoi,
+                          cauTraLoi: splitAnswers,
+                          dapAn: item.dapAn
+                      };
+  
+                      questionAnswerArray.push(questionObject);
+                  } else {
+                      console.log("'cauTraLoi' does not exist or has no value for an item.");
+                  }
+              }
+  
+              console.log("Question and answer array:", questionAnswerArray);
+              $scope.questionAnswerArray = questionAnswerArray;
+          }).catch(error => {
+              console.error("An error occurred:", error);
         });
+        
     }
 
+        
     // Gọi hàm initialize để khởi tạo dữ liệu khi trang được tải
     $scope.initialize();
 }); 
