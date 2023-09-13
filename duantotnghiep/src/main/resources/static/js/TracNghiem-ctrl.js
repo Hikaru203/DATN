f3acbe9e3ddba20e6be5b64c9718b7c979cfd042
Phuongpiper <nguyenhongphuong.vn1@gmail.com>
2023-09-13 10:12:00 +0700

    Chọn đáp án

--- duantotnghiep/src/main/resources/static/js/TracNghiem-ctrl.js
+++ duantotnghiep/src/main/resources/static/js/TracNghiem-ctrl.js
@@ -35,6 +35,38 @@ app.controller("TracNghiem-ctrl", function($scope, $http, $window) {
         });
         
     }
+    // Hàm tính điểm
+   // Hàm tính điểm
+   $scope.checkAnswers = function () {
+    $scope.diemSo = 0; // Bắt đầu với điểm số là 0
+    var chonDungItNhatMotCauHoi = false; // Biến flag để kiểm tra người dùng đã chọn đúng ít nhất một câu hỏi
+
+    for (var i = 0; i < $scope.itemsCauHoi.length; i++) {
+        var question = $scope.itemsCauHoi[i];
+        var cauTraLoi = question.cauTraLoi.trim(); // Lấy câu trả lời từ question và loại bỏ khoảng trắng
+        var dapAn = question.dapAn.trim(); // Lấy đáp án từ question và loại bỏ khoảng trắng
+
+        // Kiểm tra xem chuỗi `dapAn` có tồn tại trong chuỗi `cauTraLoi` không
+        if (cauTraLoi.includes(dapAn)) {
+            $scope.diemSo++; // Tăng điểm số nếu chuỗi `dapAn` tồn tại trong chuỗi `cauTraLoi`
+            console.log('Câu hỏi số ' + (i + 1) + ': Chọn đúng');
+            chonDungItNhatMotCauHoi = true; // Đánh dấu là người dùng đã chọn đúng ít nhất một câu hỏi
+        } else {
+            // Xử lý trường hợp chọn sai ở đây (ví dụ: in ra thông báo)
+            console.log('Câu hỏi số ' + (i + 1) + ': Chọn sai');
+        }
+    }
+
+    if (!chonDungItNhatMotCauHoi) {
+        // Xử lý trường hợp không chọn hoặc chọn sai tất cả câu hỏi ở đây (ví dụ: in ra thông báo)
+        console.log('Không chọn hoặc chọn sai tất cả câu hỏi');
+    }
+
+    alert("Điểm số của bạn là: " + $scope.diemSo);
+}
+
+
+
 
         
     // Gọi hàm initialize để khởi tạo dữ liệu khi trang được tải
