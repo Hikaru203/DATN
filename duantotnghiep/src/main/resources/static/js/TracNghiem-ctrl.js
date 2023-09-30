d48f9a359f21773db61ed69329f28838c30420ee
Phuongpiper <nguyenhongphuong.vn1@gmail.com>
2023-09-30 15:12:53 +0700

    Update

--- duantotnghiep/src/main/resources/static/js/TracNghiem-ctrl.js
+++ duantotnghiep/src/main/resources/static/js/TracNghiem-ctrl.js
@@ -12,37 +12,6 @@ app.controller("TracNghiem-ctrl", function($scope, $http, $window) {
             console.log("Dữ liệu khóa học", $scope.danhSachKhoaHoc);
         });
     };
-    $scope.initialize = function () {
-        // Gọi API để tải dữ liệu câu hỏi từ đường dẫn "/rest/TracNghiem"
-        $http.get("/rest/tracnghiem").then(resp => {
-            $scope.itemsCauHoi = resp.data;
-            console.log("api",resp.data);
-              // Tạo mảng mới chứa thông tin câu hỏi, câu trả lời và mảng đáp án
-              const questionAnswerArray = [];
-
-              for (const item of $scope.itemsCauHoi) {
-                  if (item.cauTraLoi) {
-                      const splitAnswers = item.cauTraLoi.split(',').map(answer => answer.trim());
-  
-                      // Tạo đối tượng chứa thông tin và đưa vào mảng mới
-                      const questionObject = {
-                          cauHoi: item.cauHoi,
-                          cauTraLoi: splitAnswers,
-                          dapAn: item.dapAn
-                      };
-  
-                      questionAnswerArray.push(questionObject);
-                  } else {
-                      console.log("'cauTraLoi' does not exist or has no value for an item.");
-                  }
-              }
-  
-              console.log("Question and answer array:", questionAnswerArray);
-              $scope.questionAnswerArray = questionAnswerArray;
-          }).catch(error => {
-              console.error("An error occurred:", error);
-        });
-    }
     $scope.chonKhoaHoc = function (id) {
         $http.get(`/rest/tracnghiem/${id}`).then(resp => {
             $scope.itemsCauHoi = resp.data;
@@ -75,6 +44,5 @@ app.controller("TracNghiem-ctrl", function($scope, $http, $window) {
     }
     
     // Gọi hàm initialize để khởi tạo dữ liệu khi trang được tải
-    $scope.initialize();
     $scope.loadDanhSachKhoaHoc();
 }); 
