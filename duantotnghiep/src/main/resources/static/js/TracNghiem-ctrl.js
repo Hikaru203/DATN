090362a51b0a83ae379c6593387120543a5459fe
Phuongpiper <nguyenhongphuong.vn1@gmail.com>
2023-10-02 13:54:55 +0700

    update

--- duantotnghiep/src/main/resources/static/js/TracNghiem-ctrl.js
+++ duantotnghiep/src/main/resources/static/js/TracNghiem-ctrl.js
@@ -22,26 +22,24 @@ app.controller("TracNghiem-ctrl", function($scope, $http, $window) {
               for (const item of $scope.itemsCauHoi) {
                   if (item.cauTraLoi) {
                       const splitAnswers = item.cauTraLoi.split(',').map(answer => answer.trim());
-  
                       // Tạo đối tượng chứa thông tin và đưa vào mảng mới
                       const questionObject = {
                           cauHoi: item.cauHoi,
                           cauTraLoi: splitAnswers,
                           dapAn: item.dapAn
                       };
-  
                       questionAnswerArray.push(questionObject);
                   } else {
                       console.log("'cauTraLoi' does not exist or has no value for an item.");
                   }
               }
-  
               console.log("Question and answer array:", questionAnswerArray);
               $scope.questionAnswerArray = questionAnswerArray;
           }).catch(error => {
               console.error("An error occurred:", error);
         });
     }
+    
     
     // Gọi hàm initialize để khởi tạo dữ liệu khi trang được tải
     $scope.loadDanhSachKhoaHoc();
