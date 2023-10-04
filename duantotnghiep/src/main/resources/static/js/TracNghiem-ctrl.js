880d6b64cb4db6363a3ee3d323f31b3a3c2ea5cf
Phuongpiper <nguyenhongphuong.vn1@gmail.com>
2023-10-04 14:46:38 +0700

    Update TracNghiem-ctrl.js

--- duantotnghiep/src/main/resources/static/js/TracNghiem-ctrl.js
+++ duantotnghiep/src/main/resources/static/js/TracNghiem-ctrl.js
@@ -19,7 +19,7 @@ app.controller("TracNghiem-ctrl", function ($scope, $http, $window) {
             $scope.loadDanhSachKhoaHoc = function () {
                 $http.get("/Admin/rest/KhoaHoc").then(function (response) {
                     $scope.danhSachKhoaHoc = response.data;
-                    // console.log("Dữ liệu khóa học", $scope.danhSachKhoaHoc);
+                    console.log("Dữ liệu khóa học", $scope.danhSachKhoaHoc);
                 });
             };
 
@@ -60,61 +60,35 @@ app.controller("TracNghiem-ctrl", function ($scope, $http, $window) {
                 }
             };
             // Hàm tính điểm
-            // Hàm tính điểm
-            $scope.checkAnswers = function () {
-                $scope.diemSo = 0; // Bắt đầu với điểm số là 0
-                var chonDungItNhatMotCauHoi = false; // Biến flag để kiểm tra người dùng đã chọn đúng ít nhất một câu hỏi
-
-                $scope.ketQuaCauHoi = []; // Mảng để lưu kết quả từng câu hỏi
-
-                for (var i = 0; i < $scope.itemsCauHoi.length; i++) {
-                    var question = $scope.itemsCauHoi[i];
-                    var cauTraLoi = (question.cauTraLoi || '').trim().toLowerCase(); // Lấy câu trả lời từ question và loại bỏ khoảng trắng và chuyển thành chữ thường
-
-                    var dapAn = [];
-                    console.log("Dap an:", question.dapAn);
-                    if (Array.isArray(question.dapAn)) {
-                        dapAn = question.dapAn.map(function (answer) {
-                            return answer.trim().toLowerCase(); // Loại bỏ khoảng trắng và chuyển thành chữ thường
-                        });
+            $scope.checkAnswers = function() {
+                $scope.soCauDung = 0;
+                $scope.soCauSai = 0;
+                
+                for (const question of $scope.questionAnswerArray) {
+                    if(question.selectedAnswer == undefined){             
+                        $scope.soCauDung = 0;
+                        $scope.soCauSai = 0;
+                        $scope.msg='chưa chọn hết';
+                      
+                    }else{
+                    if (question.selectedAnswer === question.dapAn) {
+                        $scope.soCauDung++;
                     } else {
-                        // Xử lý trường hợp khi dapAn không phải là mảng, ví dụ: thông báo lỗi hoặc gán một giá trị mặc định
-                        console.error('Lỗi: dapAn không phải là mảng');
-                        // Hoặc gán một giá trị mặc định cho dapAn
-                        dapAn = [];
+                        $scope.soCauSai++;
                     }
-
-                    var ketQuaCauHoi = {
-                        cauHoi: question.cauHoi,
-                        chonDung: false // Khởi tạo giá trị mặc định là sai
-                    };
-
-                    var cauTraLoiArray = cauTraLoi.split(','); // Tách câu trả lời thành một mảng các phần riêng lẻ
-
-                    var cauTraLoiDaChon = (question.selectedAnswer || '').trim().toLowerCase(); // Lấy đáp án đã chọn từ mô hình và chuyển thành chữ thường
-
-                    if (dapAn.some(function (answer) {
-                        return cauTraLoiDaChon === answer;
-                    })) { // Kiểm tra xem đáp án đã chọn có chứa trong danh sách đáp án không
-                        $scope.diemSo++; // Tăng điểm số nếu đáp án đã chọn là đúng
-                        ketQuaCauHoi.chonDung = true; // Đánh dấu là câu trả lời đúng
-                        chonDungItNhatMotCauHoi = true; // Đánh dấu là người dùng đã chọn đúng ít nhất một câu hỏi
-                    }
-
-                    $scope.ketQuaCauHoi.push(ketQuaCauHoi); // Thêm kết quả câu hỏi vào mảng
-                    console.log('Câu hỏi số ' + (i + 1) + (ketQuaCauHoi.chonDung ? ': Đúng' : ': Sai'));
+                    $scope.msg='';
                 }
-
-                if (!chonDungItNhatMotCauHoi) {
-                    // Xử lý trường hợp không chọn hoặc chọn sai tất cả câu hỏi ở đây (ví dụ: in ra thông báo)
-                    console.log('Không chọn hoặc chọn sai tất cả câu hỏi');
-                }
-
-                console.log("Điểm số của bạn là: " + $scope.diemSo); // Sử dụng console.log để hiển thị điểm số
+                console.log("A: "+question.selectedAnswer);
+                console.log("B: "+question.dapAn);
             }
-
-
-
+        
+            };
+            
+            $scope.selectAnswer = function(questionIndex, answer) {
+                $scope.selectedAnswers[questionIndex] = answer;
+                console.log(questionIndex);
+                
+            };
 
             // Gọi hàm initialize để khởi tạo dữ liệu khi trang được tải
             $scope.loadDanhSachKhoaHoc();
@@ -124,7 +98,6 @@ app.controller("TracNghiem-ctrl", function ($scope, $http, $window) {
                 console.log(questionIndex);
 
             }
-
 
         }
         );
