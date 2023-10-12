9f761e8bd9e7e55a29e9caa6f729c461af180eeb
Phuongpiper <nguyenhongphuong.vn1@gmail.com>
2023-10-12 09:35:25 +0700

    dao diện đăng nhập đăng ký mới

--- duantotnghiep/src/main/java/com/fpoly/duantotnghiep/Controller/Login_DangKy.java
+++ duantotnghiep/src/main/java/com/fpoly/duantotnghiep/Controller/Login_DangKy.java
@@ -2,6 +2,7 @@ package com.fpoly.duantotnghiep.Controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import com.fpoly.duantotnghiep.Entity.NguoiDung;
 import org.springframework.ui.Model;
@@ -12,10 +13,15 @@ public class Login_DangKy {
     @Autowired
     NguoiDungRepository NguoiDung;
 
-    @RequestMapping("/client/signin")
+    @RequestMapping("/courseOnline/dangky")
     public String showsinupFrom(Model model) {
         model.addAttribute("user", new NguoiDung());
         return "dangky";
+    }
+
+    @GetMapping("/courseOnline/dangnhap")
+    public String dangnhap() {
+        return "dangnhap";
     }
 
 }
