65516011da7fedd8c9972021c78fd4ab78966111
Nguyễn Hải Đăng <130114363+Hikaru203@users.noreply.github.com>
2023-10-19 22:52:00 +0700

    sửa upload video

--- duantotnghiep/src/main/resources/static/js/scriptLoadVideo.js
+++ duantotnghiep/src/main/resources/static/js/scriptLoadVideo.js
@@ -1,6 +1,6 @@
 const app = angular.module('loadVideo-app', ['ngCookies']);
 const videoPlaylist = document.querySelector('.video-playlist .videos');
-let videoIframe; // Thay đổi tên biến thành videoIframe
+let videoIframe;
 
 app.controller("loadVideo-app-ctrl", ['$scope', '$http', '$cookies', '$window', function ($scope, $http, $cookies, $window) {
     $scope.items = [];
@@ -8,6 +8,14 @@ app.controller("loadVideo-app-ctrl", ['$scope', '$http', '$cookies', '$window', 
     $scope.getid = function (id) {
         $window.sessionStorage.setItem('videoId', id);
         $window.location.href = '/courseOnline/video';
+    }
+
+    function setupVideoEvents($scope) {
+        // Loại bỏ sự kiện click trên các phần tử video
+        let videos = document.querySelectorAll('.video');
+        videos.forEach(selected_video => {
+            selected_video.onclick = null;
+        });
     }
 
     function changeVideo(selected_video, videos, $scope) {
@@ -21,11 +29,19 @@ app.controller("loadVideo-app-ctrl", ['$scope', '$http', '$cookies', '$window', 
 
         let match_video = $scope.data.find(video => video.id == selected_video.dataset.id);
         if (match_video) {
-            videoIframe.src = 'https://www.youtube-nocookie.com/embed/' + match_video.linkVideo + "?si=2PvjDx-NPvHeAMR3&modestbranding=1&controls=0&disablekb=1";
+            videoIframe.src = 'https://www.youtube-nocookie.com/embed/' + match_video.linkVideo + "?modestbranding=1&controls=0&disablekb=1&origin=http://localhost:8080";
             videoIframe.setAttribute("frameborder", "0");
+            // Cấu hình quyền cho iframe
             videoIframe.setAttribute("allow", "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share");
+
+            // Ẩn nút copy link bằng cách ẩn phần tử với class hoặc ID cụ thể (ví dụ: class="copy-link-button")
+            var copyLinkButton = videoIframe.contentDocument.querySelector(".ytp-copylink-button");
+            console.log(videoIframe);
+            if (copyLinkButton) {
+                copyLinkButton.style.display = "none";
+            }
             videoIframe.setAttribute("allowfullscreen", "");
-            videoIframe.title = match_video.khoaHoc.tenKhoaHoc;
+            videoIframe.title = match_video.mucLuc.khoaHoc.tenKhoaHoc;
         }
 
     }
@@ -59,7 +75,7 @@ app.controller("loadVideo-app-ctrl", ['$scope', '$http', '$cookies', '$window', 
             $scope.data.forEach((video, i) => {
                 console.log(video);
                 let video_element = `
-                    <div class="video" data-id="${video.id}">
+                    <div class="video" data-id="${video.id}" data-disabled="true">
                         <img src="/img/play.svg" alt="">
                         <p>${i + 1 > 9 ? i + 1 : '0' + (i + 1)}. </p>
                         <h3 class="title">${video.tenVideo}</h3>
@@ -68,6 +84,7 @@ app.controller("loadVideo-app-ctrl", ['$scope', '$http', '$cookies', '$window', 
                 videoPlaylist.innerHTML += video_element;
             });
 
+
             let videos = document.querySelectorAll('.video');
             setupVideoEvents($scope);
             loadFirstVideo(videos, $scope);
@@ -75,11 +92,50 @@ app.controller("loadVideo-app-ctrl", ['$scope', '$http', '$cookies', '$window', 
             console.error(error);
         });
     }
+
+    const nextVideoButton = document.getElementById('next-video');
+    const prevVideoButton = document.getElementById('prev-video');
+    let currentIndex = 0;
+
+    nextVideoButton.addEventListener('click', function () {
+        currentIndex++;
+        if (currentIndex < $scope.data.length) {
+            loadNextVideo();
+        } else {
+            // Chuyển đến trang trắc nghiệm khi ở video cuối cùng
+            redirectToQuizPage();
+        }
+    });
+
+    prevVideoButton.addEventListener('click', function () {
+        currentIndex--;
+        loadPrevVideo();
+    });
+
+    function loadNextVideo() {
+        if (currentIndex < $scope.data.length) {
+            const videos = document.querySelectorAll('.video');
+            changeVideo(videos[currentIndex], videos, $scope);
+        }
+    }
+
+    function loadPrevVideo() {
+        if (currentIndex >= 0) {
+            const videos = document.querySelectorAll('.video');
+            changeVideo(videos[currentIndex], videos, $scope);
+        }
+    }
+
+    function redirectToQuizPage() {
+        // Thay đổi URL hoặc thực hiện hành động cụ thể để chuyển đến trang trắc nghiệm
+        window.location.href = '/tracnghiem'; // Ví dụ: chuyển đến trang trắc nghiệm
+    }
+
 }]);
 
 function onPageReady() {
     console.log("Page is ready");
-    videoIframe = document.getElementById('video-iframe'); // Assign the iframe element
+    videoIframe = document.getElementById('video-iframe');
 }
 
 onPageReady();
