b189e463b0b6609a137341e7d75d05b0551d0d58
Nguyễn Hải Đăng <130114363+Hikaru203@users.noreply.github.com>
2023-11-17 20:10:24 +0700

    Squashed commit of the following:
    commit 678231448ca221d5563cfc5169e07e662aaf58c9
    Author: Nguyễn Hải Đăng <130114363+Hikaru203@users.noreply.github.com>
    Date:   Fri Nov 17 20:09:57 2023 +0700
    
        sửa code đồng bộ

--- duantotnghiep/src/main/java/com/fpoly/duantotnghiep/Controller/CheckOutController.java
+++ duantotnghiep/src/main/java/com/fpoly/duantotnghiep/Controller/CheckOutController.java
@@ -16,10 +16,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.fpoly.duantotnghiep.Entity.DangKyKhoaHoc;
 import com.fpoly.duantotnghiep.Entity.KhoaHoc;
 import com.fpoly.duantotnghiep.Entity.NguoiDung;
 import com.fpoly.duantotnghiep.Entity.ThanhToan;
 import com.fpoly.duantotnghiep.config.VNPayService;
+import com.fpoly.duantotnghiep.service.DangKyKhoaHocService;
 import com.fpoly.duantotnghiep.service.ThanhToanService;
 
 import jakarta.servlet.http.HttpServletRequest;
@@ -29,10 +31,13 @@ import jakarta.servlet.http.HttpSession;
 public class CheckOutController {
 	@Autowired
 	private VNPayService vnPayService;
-	
+
 	@Autowired
 	private ThanhToanService thanhToanService;
-	
+
+	@Autowired
+	DangKyKhoaHocService dangKyKhoaHocService;
+
 	@GetMapping("/courseOnline/CheckOut")
 	public String CheckOut() {
 		return "checkout";
@@ -51,7 +56,7 @@ public class CheckOutController {
 
 		String baseUrl = request.getScheme() + "://" + request.getServerName() + ":" + request.getServerPort();
 		String vnpayUrl = vnPayService.createOrder(orderTotal, orderInfo, baseUrl);
-		
+
 		HttpSession session = request.getSession();
 
 		if (paymentMenThod.equals("COD")) {
@@ -95,25 +100,32 @@ public class CheckOutController {
 		// Định dạng thời gian sang "dd/MM/yyyy HH:mm:ss"
 		DateTimeFormatter outputFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss");
 		String formattedPaymentTime = paymentTime.format(outputFormatter);
-		
+
 		if (paymentStatus == 1) {
-		    ThanhToan thanhToan = new ThanhToan();
+			ThanhToan thanhToan = new ThanhToan();
 
-		    // Chuyển đối tượng NguoiDung từ idNguoiDung
-		    NguoiDung nguoiDung = new NguoiDung();
-		    nguoiDung.setId(idNguoiDung);
-		    thanhToan.setNguoiDung(nguoiDung);
+			// Chuyển đối tượng NguoiDung từ idNguoiDung
+			NguoiDung nguoiDung = new NguoiDung();
+			nguoiDung.setId(idNguoiDung);
+			thanhToan.setNguoiDung(nguoiDung);
 
-		    // Chuyển đối tượng KhoaHoc từ idKhoaHoc
-		    KhoaHoc khoaHoc = new KhoaHoc();
-		    khoaHoc.setId(idKhoaHoc);
-		    thanhToan.setKhoaHoc(khoaHoc);
-		    thanhToan.setTongTien(Float.valueOf(totalPrice));
-		    thanhToan.setThoiGian(new Date());
-		    thanhToan.setTrangThai(true);
-		    thanhToanService.save(thanhToan);
+			// Chuyển đối tượng KhoaHoc từ idKhoaHoc
+			KhoaHoc khoaHoc = new KhoaHoc();
+			khoaHoc.setId(idKhoaHoc);
+			thanhToan.setKhoaHoc(khoaHoc);
+			thanhToan.setTongTien(Float.valueOf(totalPrice));
+			thanhToan.setThoiGian(new Date());
+			thanhToan.setTrangThai(true);
+			thanhToanService.save(thanhToan);
+
+			DangKyKhoaHoc dangKyKhoaHoc = new DangKyKhoaHoc();
+			dangKyKhoaHoc.setKhoaHoc(khoaHoc);
+			dangKyKhoaHoc.setNguoiDung(nguoiDung);
+			dangKyKhoaHoc.setNgayDangKy(new Date());
+			dangKyKhoaHoc.setTienDo(String.valueOf(0));
+			dangKyKhoaHoc.setTrangThai("Đang học");
+			dangKyKhoaHocService.save(dangKyKhoaHoc);
 		}
-
 
 		model.addAttribute("orderId", orderInfo);
 		model.addAttribute("totalPrice", formattedTotalAmount);
