85ad4d896605157693152da6d8526eb98f21d9b0
HuyPC03028 <huyvncst123@gmail.com>
2023-11-30 13:52:53 +0700

    update form nhà sáng tạo

--- duantotnghiep/src/main/java/com/fpoly/duantotnghiep/Controller/YouTubeController.java
+++ duantotnghiep/src/main/java/com/fpoly/duantotnghiep/Controller/YouTubeController.java
@@ -9,6 +9,7 @@ import org.springframework.web.multipart.MultipartFile;
 import org.springframework.ui.Model;
 
 import com.fpoly.duantotnghiep.Entity.LoaiKhoaHoc;
+import com.fpoly.duantotnghiep.Entity.NguoiDung;
 import com.fpoly.duantotnghiep.jparepository.LoaiKhoaHocRepository;
 import com.fpoly.duantotnghiep.service.CookieService;
 import com.fpoly.duantotnghiep.service.YouTubeService;
@@ -55,14 +56,14 @@ public class YouTubeController {
         String accessToken = (String) session.getAttribute("accessToken");
 
         if (accessToken == null) {
-            String authorizationUrl = youTubeService.getAuthorizationUrl2();
+            String authorizationUrl = youTubeService.getAuthorizationUrl();
             model.addAttribute("authorizationUrl", authorizationUrl);
             model.addAttribute("accessToken", accessToken);
             model.addAttribute("authorizationUrl", authorizationUrl);
             return "uploadKhoaHoc";
 
         } else {
-            String authorizationUrl = youTubeService.getAuthorizationUrl2();
+            String authorizationUrl = youTubeService.getAuthorizationUrl();
             model.addAttribute("accessToken", accessToken);
             model.addAttribute("authorizationUrl", authorizationUrl);
             return "uploadKhoaHoc";
@@ -74,6 +75,9 @@ public class YouTubeController {
         try {
             accessToken = youTubeService.getAccessToken(authorizationCode);
             session.setAttribute("accessToken", accessToken);
+
+            NguoiDung user = (NguoiDung) session.getAttribute("user");
+            System.out.println("user: " + user.getChucVu());
 
             // Trong phương thức khác
             String title = (String) session.getAttribute("title");
@@ -87,45 +91,17 @@ public class YouTubeController {
                 String authorizationUrl = youTubeService.getAuthorizationUrl();
                 model.addAttribute("accessToken", accessToken);
                 model.addAttribute("authorizationUrl", authorizationUrl);
-                return "Admin/Video";
-
+                if (user.getChucVu().equals("Admin")) {
+                    return "redirect:/Admin/Video";
+                } else {
+                    return "redirect:/courseOnline/uploadKhoaHoc";
+                }
             } catch (Exception e) {
                 return "redirect:/Admin/Video";
             }
         } catch (Exception e) {
             // Handle exceptions
             return "redirect:/Admin/Video";
-        }
-
-    }
-
-
-    @GetMapping("/oauth2callback2")
-    public String oauth2callback2(@RequestParam("code") String authorizationCode, HttpSession session, Model model) {
-        try {
-            accessToken = youTubeService.getAccessToken(authorizationCode);
-            session.setAttribute("accessToken", accessToken);
-
-            // Trong phương thức khác
-            String title = (String) session.getAttribute("title");
-            String description = (String) session.getAttribute("description");
-            String privacyStatus = (String) session.getAttribute("privacyStatus");
-            MultipartFile file = (MultipartFile) session.getAttribute("file");
-
-            try {
-                youTubeService.uploadVideo(title, description, privacyStatus, file, accessToken);
-
-                String authorizationUrl = youTubeService.getAuthorizationUrl2();
-                model.addAttribute("accessToken", accessToken);
-                model.addAttribute("authorizationUrl", authorizationUrl);
-                return "uploadKhoaHoc";
-
-            } catch (Exception e) {
-                return "redirect:/courseOnline/uploadKhoaHoc";
-            }
-        } catch (Exception e) {
-            // Handle exceptions
-            return "redirect:/courseOnline/uploadKhoaHoc";
         }
 
     }
