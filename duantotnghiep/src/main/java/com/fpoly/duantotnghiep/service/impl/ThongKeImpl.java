86946daaa3f9f414eb73cb3c56886e2816bbde37
Nguyễn Hải Đăng <130114363+Hikaru203@users.noreply.github.com>
2023-12-13 17:25:11 +0700

    Squashed commit of the following:
    commit c47ff564c4c1414e05ea715f7ba3d5cabbb45fd3
    Author: haovmpc03048 <133337015+haovmpc03048@users.noreply.github.com>
    Date:   Wed Dec 13 17:24:07 2023 +0700
    
        Hoàn thành thống kê tháng này, cả năm, năm trước, Chức năng tìm kiếm cho khóa học
    
    commit 220e90e0d33f5e17d824fb7c350696fdfffda922
    Author: haovmpc03048 <133337015+haovmpc03048@users.noreply.github.com>
    Date:   Wed Dec 13 02:51:44 2023 +0700
    
        hoàn thành doanh thu trong ngày và sửa giao diện thống kê
    
    commit 053816ba47c4393d476d5e4aece20cb544bdda23
    Merge: 0e3fd02 21f3528
    Author: haovmpc03048 <133337015+haovmpc03048@users.noreply.github.com>
    Date:   Tue Dec 12 21:31:00 2023 +0700
    
        Merge branch 'dev' into Hào
    
    commit 0e3fd02e86cf6ccb75c7a20cd2061b5651c5cb31
    Author: haovmpc03048 <133337015+haovmpc03048@users.noreply.github.com>
    Date:   Thu Dec 7 22:56:54 2023 +0700
    
        Fix giao diện
    
    commit b43d9f122864e8a74239378bbef17151251eaadb
    Merge: 152ab82 3474bf4
    Author: haovmpc03048 <133337015+haovmpc03048@users.noreply.github.com>
    Date:   Thu Dec 7 20:26:40 2023 +0700
    
        Merge branch 'dev' into Hào
    
    commit 152ab82caef06786a312fdd6e4f1a82993167947
    Author: haovmpc03048 <133337015+haovmpc03048@users.noreply.github.com>
    Date:   Thu Dec 7 20:12:09 2023 +0700
    
        Sửa code

--- duantotnghiep/src/main/java/com/fpoly/duantotnghiep/service/impl/ThongKeImpl.java
+++ duantotnghiep/src/main/java/com/fpoly/duantotnghiep/service/impl/ThongKeImpl.java
@@ -95,4 +95,72 @@ ThanhToanRepository thanhToanRepository;
     public long countUsersInKhoaHocTG(Long idKhoaHoc,String batDau, String ketThuc) {
         return dangKyKhoaHocRepository.countByKhoaHocIdTG(idKhoaHoc, batDau, ketThuc);
     }
+
+    @Override
+    public List<ThongKeDATA> ThongKeTheoNgay() {
+        List<Object[]> result = thanhToanRepository.ThongKeTrongNgay();
+        List<ThongKeDATA> revenueDataList = new ArrayList<>();
+
+        for (Object[] row : result) {
+            ThongKeDATA revenueData = new ThongKeDATA();
+            revenueData.setYear((int) row[0]);
+            revenueData.setMonth((int) row[1]);
+            revenueData.setTotalRevenue((BigDecimal) row[2]);
+
+            revenueDataList.add(revenueData);
+        }
+
+        return revenueDataList;
+    }
+
+    @Override
+    public List<ThongKeDATA> ThongKeThangNay() {
+        List<Object[]> result = thanhToanRepository.ThongKeThangNay();
+        List<ThongKeDATA> revenueDataList = new ArrayList<>();
+
+        for (Object[] row : result) {
+            ThongKeDATA revenueData = new ThongKeDATA();
+            revenueData.setYear((int) row[0]);
+            revenueData.setMonth((int) row[1]);
+            revenueData.setTotalRevenue((BigDecimal) row[2]);
+
+            revenueDataList.add(revenueData);
+        }
+
+        return revenueDataList;
+    }
+
+    @Override
+    public List<ThongKeDATA> ThongKeNamNay() {
+        List<Object[]> result = thanhToanRepository.ThongKeNamNay();
+        List<ThongKeDATA> revenueDataList = new ArrayList<>();
+
+        for (Object[] row : result) {
+            ThongKeDATA revenueData = new ThongKeDATA();
+            revenueData.setYear((int) row[0]);
+            revenueData.setMonth((int) row[1]);
+            revenueData.setTotalRevenue((BigDecimal) row[2]);
+
+            revenueDataList.add(revenueData);
+        }
+
+        return revenueDataList;
+    }
+
+     @Override
+    public List<ThongKeDATA> ThongKeNamTruoc() {
+        List<Object[]> result = thanhToanRepository.ThongKeNamTruoc();
+        List<ThongKeDATA> revenueDataList = new ArrayList<>();
+
+        for (Object[] row : result) {
+            ThongKeDATA revenueData = new ThongKeDATA();
+            revenueData.setYear((int) row[0]);
+            revenueData.setMonth((int) row[1]);
+            revenueData.setTotalRevenue((BigDecimal) row[2]);
+
+            revenueDataList.add(revenueData);
+        }
+
+        return revenueDataList;
+    }
 }
